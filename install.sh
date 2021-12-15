2ea1f16e0a5f9ca1c6c1c70cb749384c195e1b26
Marcelo Alvarez <marcelo.alvarez@berkeley.edu>
2021-12-14 17:18:49 -0800

    removed get_pyversion.py

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ curl -SL $MINICONDA \
   && /bin/bash miniconda.sh -b -f -p $CONDADIR
 
 source $CONDADIR/bin/activate
-export PYVERSION=`python get_version.py`
+export PYVERSION=$(python -c "import sys; print(str(sys.version_info[0])+'.'+str(sys.version_info[1]))")
 echo Using Python version $PYVERSION
 
 # Install packages
